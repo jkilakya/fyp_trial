a294fbc3c65b7d865f8890a533acb24650af40a7
devchoradia <62145388+devchoradia@users.noreply.github.com>
2023-01-24 15:24:50 +0530

    Add files via upload

---
+++ LOGIN2/LOGIN2/LOGIN2App.swift
@@ -0,0 +1,17 @@
+//
+//  LOGIN2App.swift
+//  LOGIN2
+//
+//  Created by Choradia Dev on 23/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct LOGIN2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
